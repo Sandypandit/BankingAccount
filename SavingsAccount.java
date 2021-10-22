7a88d94a9600a90eb0aa6aaaea867bba24f229c4
Sandypandit <sandypandit86@gmail.com>
2021-10-22 08:30:45 +0530

    Committing class SavingsAccount

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -1,5 +1,28 @@
+package com.bridgelabz;
 
 class SavingsAccount{
+	
+	static int annualInterestRate;
+    private int savingsBalance;
+
+    public void setSavingsBalance(int savingsBalance){
+        this.savingsBalance = savingsBalance;
+    }
+
+    public int getSavingsBalance(){
+        return savingsBalance;
+    }
+
+    public void calculateMonthlyInterest(){
+        int monthlyInterest = (savingsBalance*annualInterestRate)/12;
+    }
+
+    public void modifyInterestRate(int annualInterestRate){
+        this.annualInterestRate = annualInterestRate;
+    }
+
+
+	
 	public static void main(String[] args) {
 		
 	}
